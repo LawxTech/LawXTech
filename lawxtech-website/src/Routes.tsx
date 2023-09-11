53cc3553e2d46eb20637996c461fa9fb0127713c
LawxTech <lawxtechseries@gmail.com>
2023-09-11 12:36:50 -0400

    updated the imports

--- lawxtech-website/src/Routes.tsx
+++ lawxtech-website/src/Routes.tsx
@@ -4,20 +4,20 @@ import { BrowserRouter as Router, Route, Routes as ReactRoutes } from 'react-rou
 
 import Home from './pages/Home';
 import About from './pages/About';
-import Services from './pages/Series';
+import Series from './pages/Series';
 import Contact from './pages/Contact';
 
 const Routes: React.FC = () => {
   return (
     <Router>
-      <ReactRoutes> {/* Replace Switch with ReactRoutes */}
-        <Route path="/" element={<Home />} /> {/* Use "element" instead of "component" */}
+      <ReactRoutes>
+        <Route path="/" element={<Home />} /> 
         <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/services" element={<Series />} />
         <Route path="/contact" element={<Contact />} />
       </ReactRoutes>
     </Router>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
