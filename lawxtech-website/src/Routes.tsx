f2b32bdbf1b3fa21acd0c8436b1fc0b5dcac8f42
LawxTech <lawxtechseries@gmail.com>
2023-09-13 14:12:14 -0400

    removed router from routes page

--- lawxtech-website/src/Routes.tsx
+++ lawxtech-website/src/Routes.tsx
@@ -1,22 +1,19 @@
-// Routes.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes as ReactRoutes } from 'react-router-dom'; // Update the import
+import React from 'react'
+import { Route, Routes as ReactRoutes } from 'react-router-dom'
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Series from './pages/Series';
-import Contact from './pages/Contact';
+import Home from './pages/Home'
+import About from './pages/About'
+import Series from './pages/Series'
+import Contact from './pages/Contact'
 
 const Routes: React.FC = () => {
   return (
-    <Router>
       <ReactRoutes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Series />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/about-us" element={<About />} />
+        <Route path="/series" element={<Series />} />
+        <Route path="/contact-us" element={<Contact />} />
       </ReactRoutes>
-    </Router>
   )
 }
 
