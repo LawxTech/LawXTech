778145405f655f7f69c29a292bb15546a64b3a10
LawxTech <lawxtechseries@gmail.com>
2023-10-13 09:41:01 -0400

    fixed the message input box

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -30,19 +30,69 @@ const useStyles = makeStyles({
 
 const Contact: React.FC = () => {
   const classes = useStyles()
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value)
+  }
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value)
+  }
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    setEmail(e.target.value)
+  }
+
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value)
+  }
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
+
+  const resetForm = () => {
+    setFirstName('')
+    setLastName('')
+    setEmail('')
+    setPhoneNumber('')
+    setMessage('')
   };
 
   const subscribe = () => {
-    // Construct a mailto link with the user's email address
-    const mailtoLink = `mailto:rofiatolusanya12@gmail.com?subject=Subscribe&body=Email: ${email}`;
+    const formData = {
+      'First Name': firstName,
+      'Last Name': lastName,
+      'Email': email,
+      'Phone Number': phoneNumber,
+      'Message': message,
+    };
 
-    // Open the user's email client with the pre-filled email
-    window.location.href = mailtoLink;
-  };
+    fetch('https://formsubmit.co/rofiatolusanya12@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert('Subscription successful!')
+          resetForm()
+        } else {
+          alert('Subscription failed. Please try again later.')
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+        alert('Subscription failed. Please try again later.')
+      })
+  }
 
   return (
     <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '7em'}}>
@@ -84,6 +134,8 @@ const Contact: React.FC = () => {
             variant="outlined"
             margin="dense"
             size="small"
+            value={firstName}
+            onChange={handleFirstNameChange}
             InputLabelProps={{
               className: classes.whiteInputLabel
             }}
@@ -97,6 +149,8 @@ const Contact: React.FC = () => {
             variant="outlined"
             margin="dense"
             size="small"
+            value={lastName}
+            onChange={handleLastNameChange}
             InputLabelProps={{
               className: classes.whiteInputLabel
             }}
@@ -111,6 +165,8 @@ const Contact: React.FC = () => {
             variant="outlined"
             margin="dense"
             size="small"
+            value={email}
+            onChange={handleEmailChange}
             InputLabelProps={{
               className: classes.whiteInputLabel
             }}
@@ -124,7 +180,8 @@ const Contact: React.FC = () => {
             variant="outlined"
             margin="dense"
             size="small"
-            value={email}
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
             InputLabelProps={{
               className: classes.whiteInputLabel
             }}
@@ -134,8 +191,8 @@ const Contact: React.FC = () => {
           />
         </Box>
         <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
-          <Input id="standard-adornment-amount" />
+          <InputLabel htmlFor="standard-adornment-amount">Message</InputLabel>
+          <Input id="standard-adornment-amount" value={message} onChange={handleMessageChange} />
         </FormControl>
         <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button type="primary" onClick={subscribe}>
