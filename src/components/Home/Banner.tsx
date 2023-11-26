0e00d860bb21200409c7127f4fc88a2488f54227
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-26 23:55:39 +0100

    update Banner.tsx

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,64 +1,88 @@
 import { Link } from "react-router-dom";
-import { Typography, Grid, Box, Container} from "@mui/material";
-import { ThemeProvider } from '@mui/system';
+import { ThemeProvider } from "@mui/system";
 import theme from "../../styles/theme";
 import Button from "@mui/material/Button";
-import "../.../../../App.css"
-import React from "react";
-
-  
+import Container from "@mui/material/Container";
 
 export const Banner = () => {
-    return(
-        <ThemeProvider theme = {theme}> 
-            <Box maxWidth="xl" className="banner">
-                <Container maxWidth="xl" sx={{ display: 'flex', justifyContent:"space-between", alignItems:"center" }}>
-                    <Box maxWidth="sm">
-                        <Typography variant="h3" letterSpacing={8} fontWeight="bold" sx={{fontSize:"3rem"}} pt="4rem" px={4} ml={4} mt={0}>
-                            Empowering
-                            <span>
-                                <Typography variant="h3" letterSpacing={8} fontWeight="bold" sx={{fontSize:"3rem"}}>Lawyers in the</Typography> 
-                            </span>
-                        </Typography>
-                        <Typography variant="h3" letterSpacing={8} color="#42858C" sx={{fontSize:"2.8rem"}}fontWeight="bold" fontStyle="oblique" ml={4} px={4} mb={2}>
-                            Tech World
-                        </Typography>
-                        <Grid container maxWidth="md">
-                            <Grid item maxWidth="md">
-                                <Box maxWidth="sm">
-                                    <Typography variant="h6" sx={{fontSize:".98rem"}} ml={4} px={4}  paragraph>
-                                        Are you a lawyer looking to embrace the dynamic world of technology? 
-                                        Or you are a techie seeking legal knowledge to boost your career? &nbsp;
-                                        Law x Tech is your gateway to a thriving career at the intersection of law and technology.
-                                        This is where Law meets Tech.
-                                    </Typography>
-                                </Box>
-                                <Box id="cta">
-                                    <Link to="https://join.slack.com/t/lawxtech/shared_invite/zt-20u7mvfqu-EWVec2Qip3XhYoUyVtyvpA" target="_blank">
-                                        <Button className="btn"
-                                            variant="contained"
-                                            size="large"
-                                        >
-                                            Join Community
-                                        </Button>
-                                    </Link>
-                                </Box>
-                            </Grid>
-                        </Grid> 
-                    </Box>
-                    <Box >
-                        <Grid container maxWidth="sm" sx={{ justifyContent:"center", alignItems:"center" }}  className="reverse">
-                            <Grid item maxWidth="sm">
-                                <Box component="img" sx={{marginRight:"40px", paddingRight:"30px"}}
-                                    alt="Hero image"
-                                    src={ require("../../assets/hero.png")}
-                                    maxWidth="sm"
-                                />
-                            </Grid>  
-                        </Grid>
-                    </Box>
-                </Container>
-            </Box>
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="w-full h-[70vh] border-b">
+        <div
+          style={{
+            width: "100%",
+            height: "70vh",
+            background: `url("/images/about.jpeg")`,
+            boxShadow: "inset 0 0 0 1000px rgba(0, 0, 0, 0.7)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+          }}
+        >
+          <div className="h-[70vh] flex items-center w-full md:w-[700px] p-8 mx-auto">
+            <div className="grid grid-cols-1 gap-[1.3rem] ">
+              <h1 className="text-[2.5rem] md:text-[3.5rem] md:!leading-[4rem] font-[600] text-[#fff] sm:text-center ">
+                Empowering Lawyers in the Tech World.
+              </h1>
+
+              <p className="text-[1rem] sm:text-center text-neutral-200 font-[500] ">
+                Are you a lawyer looking to embrace the dynamic world of
+                technology? Or you are a techie seeking legal knowledge to boost
+                your career? &nbsp; Law x Tech is your gateway to a thriving
+                career at the intersection of law and technology.
+              </p>
+
+              <Link
+                to="https://join.slack.com/t/lawxtech/shared_invite/zt-20u7mvfqu-EWVec2Qip3XhYoUyVtyvpA"
+                target="_blank"
+                className=" sm:text-center"
+              >
+                <Button
+                  className="btn"
+                  variant="contained"
+                  size="large"
+                  sx={{
+                    padding: "1rem 2rem",
+                  }}
+                >
+                  Join Our Community
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Container maxWidth="lg">
+        <div className="py-14">
+          <div className="grid lg:grid-cols-4 grid-cols-2 gap-4">
+            <div className="flex flex-col items-center justify-center p-4">
+              <p className="text-[.95rem] text-center ">Mini Series</p>
+              <h1 className="text-[2.5rem] !leading-[3.6rem] font-[600] text-[#113167] text-center ">
+                22
+              </h1>
+            </div>
+            <div className="flex flex-col items-center justify-center p-4">
+              <p className="text-[.95rem] text-center ">Major Seies</p>
+              <h1 className="text-[2.5rem] !leading-[3.6rem] font-[600] text-[#113167] text-center ">
+                12
+              </h1>
+            </div>
+            <div className="flex flex-col items-center justify-center p-4">
+              <p className="text-[.95rem] text-center ">Current Members</p>
+              <h1 className="text-[2.5rem] !leading-[3.6rem] font-[600] text-[#113167] text-center ">
+                300+
+              </h1>
+            </div>
+            <div className="flex flex-col items-center justify-center p-4">
+              <p className="text-[.95rem] text-center ">Team Members</p>
+              <h1 className="text-[2.5rem] !leading-[3.6rem] font-[600] text-[#113167] text-center ">
+                10
+              </h1>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+};
