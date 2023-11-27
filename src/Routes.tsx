66deb7126b4db8ff88fff713e106b2d6b5dd91c8
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-27 22:05:03 +0100

    update Routes.tsx and Header.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,20 @@
-import React from "react"
-import { Route, Routes as ReactRoutes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Series from "./pages/Series"
-import Contact from "./pages/Contact"
+import React from "react";
+import { Route, Routes as ReactRoutes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Series from "./pages/Series";
+import Contact from "./pages/Contact";
 
 const Routes: React.FC = () => {
   return (
-      <ReactRoutes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about-us" element={<About />} />
-        <Route path="/series" element={<Series />} />
-        <Route path="/contact-us" element={<Contact />} />
-      </ReactRoutes>
-  )
-}
+    <ReactRoutes>
+      <Route path="/LawXTech" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about-us" element={<About />} />
+      <Route path="/series" element={<Series />} />
+      <Route path="/contact-us" element={<Contact />} />
+    </ReactRoutes>
+  );
+};
 
-export default Routes
+export default Routes;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function DrawerAppBar() {
                 navigate(
                   `/${
                     item === "Home"
-                      ? ""
+                      ? "LawXTech"
                       : item.toLowerCase().replace(/\s+/g, "-")
                   }`
                 );
