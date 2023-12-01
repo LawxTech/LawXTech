8ca3fe9cbdcb786822b476fb752bcb02d51dcbef
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-01 16:32:33 +0100

    update MissionAndVission.tsx

--- src/components/About/MissionAndVission.tsx
+++ src/components/About/MissionAndVission.tsx
@@ -1,27 +1,19 @@
-import { Box, Container, Typography } from "@mui/material";
-import React from "react";
+import { Container } from "@mui/material";
 
 const MIssionAndVission = () => {
   return (
     <>
       <section
-        style={{
-          padding: "7.38rem 4.38rem",
-          backgroundColor: "#113167",
-          color: "#fff",
-        }}
+        className="py-[5rem] px-[1.5rem] lg:py-[7.38rem] lg:px-[4.38rem] bg-[#113167] text-white"
+        id="mission"
       >
         <Container maxWidth="lg">
-          <Box>
-            <Box>
-              <Typography variant="h2">Our Mission</Typography>
-              <Typography
-                variant="body1"
-                sx={{
-                  color: "#fff",
-                  lineHeight: "1.7rem",
-                }}
-              >
+          <div>
+            <div>
+              <h2 className="text-[2rem] lg:text-[2.8rem] font-bold ">
+                Our Mission
+              </h2>
+              <p className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem]  text-white mt-[1rem]">
                 At #LawxTech, our mission is to bridge the gap between the
                 worlds of law and technology, providing a dynamic platform where
                 lawyers and law students can explore, learn, and thrive in the
@@ -30,30 +22,22 @@ const MIssionAndVission = () => {
                 networking, we empower our participants to embrace innovation,
                 cultivate expertise, and pioneer positive change at the
                 intersection of law and technology.{" "}
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                marginTop: "4rem",
-              }}
-            >
-              <Typography variant="h2">Our Vision</Typography>
-              <Typography
-                variant="body1"
-                sx={{
-                  color: "#fff",
-                  lineHeight: "1.7rem",
-                }}
-              >
+              </p>
+            </div>
+            <div className="mt-[3rem] md:mt-[4rem]">
+              <h2 className="text-[2.3rem] lg:text-[2.8rem] font-bold ">
+                Our Vision
+              </h2>
+              <p className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem]  text-white mt-[1rem]">
                 Our vision is to pioneer a transformed legal profession by
                 empowering legal minds with the skills to navigate technology.
                 We envision tech-savvy lawyers who integrate innovation and
                 drive impact, shaping a collaborative future in law and tech for
                 lawyers and/or law students whether rooted in law or aspiring to
                 transition to tech, with #LawxTechSeries as the catalyst.
-              </Typography>
-            </Box>
-          </Box>
+              </p>
+            </div>
+          </div>
         </Container>
       </section>
     </>
