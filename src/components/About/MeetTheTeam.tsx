4fdc076dfc8ddf2c2f147ba43884af63f7bbab3d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-01-09 07:44:28 +0100

    Update team member name and image

--- src/components/About/MeetTheTeam.tsx
+++ src/components/About/MeetTheTeam.tsx
@@ -6,16 +6,15 @@ const teamMembers = [
     position: "Founder",
     image: "Sopuruchi Rufus, Founder .jpeg",
   },
-
   {
     name: "Joel",
     position: "Graphics Designer",
     image: "Joel, Graphics Designer.jpeg",
   },
   {
-    name: "Ezinne",
+    name: "Favour",
     position: "Community Manager",
-    image: "Ezinne, Social Media Manager Vounteer.jpeg",
+    image: "Favour.jpeg",
   },
   {
     name: "Daniel",
@@ -42,30 +41,32 @@ const teamMembers = [
     position: "Email Marketer ",
     image: "BAKARE ADEFOLAKEMI, Email Marketer Volunteer.JPG",
   },
-  {
-    name: "Aniefiok Michael",
-    position: "UI/UX",
-    image: "Aniefiok Michael, UI_UX Volunteer .jpeg",
-  },
+  // {
+  //   name: "Aniefiok Michael",
+  //   position: "UI/UX",
+  //   image: "Aniefiok Michael, UI_UX Volunteer .jpeg",
+  // },
 ];
 
 const MeetTeam = () => {
   return (
     <div>
       <Container maxWidth="lg">
-        <section className="bg-white p-16">
+        <section className="bg-white py-8 px-[1.5rem] md:p-16">
           <div className="container mx-auto">
-            <h4 className="text-2xl text-blue-800 text-center mb-8 font-bold mt-4">
+            <h4 className="text-2xl text-[#113167] text-center mb-8 font-bold mt-4">
               Meet the Team
             </h4>
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 md:gap-6 items-center justify-center mb-4">
               {teamMembers.map((member, index) => (
-                <div className="mb-6 flex lg:block" key={index}>
-                  <img
-                    src={`/assets/members/${member.image}`}
-                    alt={`Team member {{ $index + 1 }}`}
-                    className="w-24 h-24 lg:w-60 lg:h-60 rounded-lg"
-                  />
+                <div className="mb-6 flex items-center lg:block" key={index}>
+                  <div className=" overflow-hidden rounded-lg h-[6rem] !w-[6rem] flex-shrink-0 lg:w-60 lg:h-60">
+                    <img
+                      src={`/assets/members/${member.image}`}
+                      alt={`Team member : ${member.name}`}
+                      className=" rounded-lg object-center object-fill"
+                    />
+                  </div>
                   <div className="w-full p-2">
                     <h6 className="text-sm md:text-lg  mt-2">
                       {member.position}
