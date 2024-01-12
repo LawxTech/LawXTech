2cf63c94b6c3fb860f2795f308581939b7403a34
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-01-12 11:35:50 +0100

    Refactor mission and vision headings and paragraphs

--- src/components/About/MissionAndVission.tsx
+++ src/components/About/MissionAndVission.tsx
@@ -10,10 +10,16 @@ const MIssionAndVission = () => {
         <Container maxWidth="lg">
           <div>
             <div>
-              <h2 className="text-[2rem] lg:text-[2.8rem] font-bold ">
+              <h2
+                data-aos="fade-left"
+                className="text-[2rem] lg:text-[2.8rem] font-bold "
+              >
                 Our Mission
               </h2>
-              <p className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem] text-justify text-white mt-[1rem]">
+              <p
+                data-aos="fade-up"
+                className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem] text-justify text-white mt-[1rem]"
+              >
                 At LawxTech, our mission is to bridge the gap between the worlds
                 of law and technology, providing a dynamic platform where
                 lawyers and law students can explore, learn, and thrive in the
@@ -25,10 +31,16 @@ const MIssionAndVission = () => {
               </p>
             </div>
             <div className="mt-[3rem] md:mt-[4rem]">
-              <h2 className="text-[2.3rem] lg:text-[2.8rem] font-bold ">
+              <h2
+                data-aos="fade-left"
+                className="text-[2.3rem] lg:text-[2.8rem] font-bold "
+              >
                 Our Vision
               </h2>
-              <p className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem] text-justify text-white mt-[1rem]">
+              <p
+                data-aos="fade-up"
+                className="text-[0.875rem] font-[500] md:text-[1rem] leading-[1.7rem] text-justify text-white mt-[1rem]"
+              >
                 Our vision is to pioneer a transformed legal profession by
                 empowering legal minds with the skills to navigate technology.
                 We envision tech-savvy lawyers who integrate innovation and
