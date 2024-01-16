67cc012711d9a5ac4e2b647b7e45c97aea2fb4f7
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-01-16 22:56:06 +0100

    Refactor DisplayCard component styling

--- src/components/shared/DisplayCard/DisplayCard.tsx
+++ src/components/shared/DisplayCard/DisplayCard.tsx
@@ -22,40 +22,33 @@ const DisplayCard = ({ display }: Props) => {
     <>
       <Link to={display.link}>
         <div
-          className={`border dark:border-neutral-700 rounded-xl w-full items-center flex flex-col hover:border-neutral-600  `}
+          className={`border hover:border-[#113167] hover:cursor-pointer rounded-xl w-full items-center flex flex-col  `}
         >
           <div
-            className={`w-full rounded-t-xl h-[16rem] bg-transparent dark:bg-neutral-800/50 flex items-center justify-center `}
+            className={`w-full rounded-t-xl h-[16rem] bg-transparent flex items-center justify-center `}
           >
             <img
-              src={`/assets/images/placeholder_image.png`}
-              className="object-cover w-full h-full rounded-xl"
-              alt="An SVG illustration of a person dressed in different clothings"
+              src={display.image}
+              className="object-cover w-full h-full rounded-t-xl"
+              alt={display.title}
             />
           </div>
-          <div
-            className={` w-full rounded-b-xl min-h-[14rem] py-6 px-4 dark:text-neutral-400 `}
-          >
-            <div className="mb-2 flex justify-between ">
-              <p className="text-sm ">Toluwalope Akinkunmi</p>
+          <div className={` w-full rounded-b-xl min-h-[14rem] p-4  `}>
+            <div className="mb-1 flex justify-between ">
+              <p className="text-[0.75rem] ">LawxTech</p>
             </div>
             <h1
-              className={` text-xl md:text-[1.4rem] mb-2 font-[500] text-[#000] dark:text-neutral-200 line-clamp-2`}
+              className={` text-xl md:text-[1.4rem] mb-2 font-[500] text-[#000]  line-clamp-3`}
             >
               {display.title}
             </h1>
-            <p className="mb-1 sm:mb-2 line-clamp-3 !text- text-sm text-neutral-600  ">
-              {display.description}
+            <p className="mb-1 sm:mb-2 line-clamp-2 !text- text-[0.85rem] text-neutral-600  ">
+              {display.article[0].p}
             </p>
-            <div className="mt-3 flex items-center space-x-2 text-neutral-500 font-[400] font-sans ">
+            <div className="mt-3 flex items-center space-x-2 text-neutral-500 font-[400] text-[0.9rem]  ">
               <span className="text-sm ">{display.date}</span>
               <span className="text-sm ">.</span>
               <span className="text-sm">{display?.duration}</span>
-              <span className="text-sm ">.</span>
-              <p className="font-raleway font-normal text-sm text-neutral-600 flex items-center space-x-2">
-                <i className="fa-solid fa-hands-clapping fa-md fa-fw"></i>
-                <span>2</span>
-              </p>
             </div>
           </div>
         </div>
